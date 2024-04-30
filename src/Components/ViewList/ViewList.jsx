8ad095406106a56b59ad8c27d901d3ccaf468944
nokibh <nokibhowlader2003@gmail.com>
2024-04-30 16:45:50 +0600

    added delete operation

--- src/Components/ViewList/ViewList.jsx
+++ src/Components/ViewList/ViewList.jsx
@@ -1,6 +1,37 @@
+import Swal from 'sweetalert2';
+
 const ViewList = ({ item }) => {
-  console.log(item);
+  // console.log(item);
   const { _id, name, country, location } = item;
+  const handleDelete = _id => {
+    console.log(_id);
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then(result => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/mylist/${_id}`, {
+          method: 'DELETE',
+        })
+          .then(res => res.json())
+          .then(data => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: 'Deleted!',
+                text: 'Your Location has been deleted.',
+                icon: 'success',
+              });
+            }
+          });
+      }
+    });
+  };
   return (
     <div>
       <div className="overflow-x-auto mb-4">
@@ -25,7 +56,7 @@ const ViewList = ({ item }) => {
                 <button>Update</button>
               </th>
               <th>
-                <button>Delete</button>
+                <button onClick={() => handleDelete(_id)}>Delete</button>
               </th>
             </tr>
           </tbody>
